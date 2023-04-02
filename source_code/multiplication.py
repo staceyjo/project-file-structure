c1cca712a75ece26af5d4e6d22c42444611b6a7d
Stacey <stacey.l.joseph@gmail.com>
2023-04-01 23:52:35 -0400

    updates file structure

---
+++ source_code/multiplication.py
@@ -0,0 +1,7 @@
+import source_code.addition as addition
+
+def perform_operation(multiplier, multiplicand):
+    result = 0
+    for _ in range(multiplier):
+        result = addition.perform_operation(result, multiplicand)
+    return result
